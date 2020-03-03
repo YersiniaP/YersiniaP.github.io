1ee88daa326037ad2ef2419e9b9fd637289cac89
YersiniaP <51828255+YersiniaP@users.noreply.github.com>
2020-03-02 21:52:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
       document.addEventListener('DOMContentLoaded', bindButtons);
 
       function bindButtons(){
-        document.getElementById('stringSubmit').addEventListener('click', function(event){
+        document.getElementById('emailSubmit').addEventListener('click', function(event){
           var req = new XMLHttpRequest();
           var payload = {input:null};
         
@@ -11,11 +11,11 @@
 
 
           req.open('POST', 'http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', false);
-          req.setRequestHeader('Content-Type', 'application/json');
+          req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
           req.addEventListener('load',function(){
             if(req.status >= 200 && req.status < 400){
               var response = JSON.parse(req.responseText);
-              document.getElementById('originalString').textContent = JSON.stringify(response);
+              document.getElementById('emailString').textContent = JSON.stringify(response);
             }
             else{
               console.log("Error in network request: " + req.statusText);
