88679b16e4a3fcac4f33576b1020eaf3479d9cde
YersiniaP <51828255+YersiniaP@users.noreply.github.com>
2020-03-02 21:08:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ var apiKey = '41a24f1f0a49193e9c9058894dd610f4';
 document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons() {
-    // add event listenter for the weather button
-    document.getElementById('submitWeather').addEventListener('click', function (event) {
+    document.getElementById("submitWeather").addEventListener('click', function (event) {
         var req = new XMLHttpRequest(),
             city = document.getElementById('city').value,
             country = document.getElementById('country').value,
             zip = document.getElementById('zip').value;
-
-        // if zip field is not null, then use it for the api
         if (zip) {
             req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us"
                 + "&appid=" + apiKey, true);
