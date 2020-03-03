910ff5aa9743e85b605b1670f2cf844deab45ebd
YersiniaP <51828255+YersiniaP@users.noreply.github.com>
2020-03-02 20:20:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var apiKey = '41a24f1f0a49193e9c9058894dd610f4';
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+  document.getElementById('weather').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+    //var payload = {zipCode:null};
+    //payload.zipCode = document.getElementById('zipCode').value;
+    //console.log(payload.zipCode);
+    req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=paris,fr&appid=' + apiKey, false);
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+                document.getElementById('cityResults').textContent = request.name;
+                document.getElementById('temperatureResults').textContent = request.main.temp;
+                document.getElementById('conditionResults').textContent = request.weather[0].description;
+      }
+      else{
+        console.log("Error in network request: " + req.statusText);
+      }});
+
+    req.send(null);
+
+    event.preventDefault();
+  });
+}
