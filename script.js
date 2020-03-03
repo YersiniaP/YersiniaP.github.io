855fdd62c54795f49c2a44e86157777066319a19
YersiniaP <51828255+YersiniaP@users.noreply.github.com>
2020-03-02 20:59:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var apiKey = '41a24f1f0a49193e9c9058894dd610f4';
-
 document.addEventListener('DOMContentLoaded', bindButtons);
 
-function bindButtons(){
-  document.getElementById('weather').addEventListener('click', function(event){
-    var req = new XMLHttpRequest();
-    //var payload = {zipCode:null};
-    //payload.zipCode = document.getElementById('zipCode').value;
-    //console.log(payload.zipCode);
-    req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=paris,fr&appid=' + apiKey, false);
-    req.addEventListener('load',function(){
-      if(req.status >= 200 && req.status < 400){
-        var response = JSON.parse(req.responseText);
+function bindButtons() {
+    // add event listenter for the weather button
+    document.getElementById('submitWeather').addEventListener('click', function (event) {
+        var req = new XMLHttpRequest(),
+            city = document.getElementById('city').value,
+            country = document.getElementById('country').value,
+            zip = document.getElementById('zip').value;
+
+        // if zip field is not null, then use it for the api
+        if (zip) {
+            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us"
+                + "&appid=" + apiKey, true);
+        } else {
+            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country
+                + "&appid=" + apiKey, true);
+        }
+
+        req.addEventListener('load', function () {
+            if (req.status >= 200 && req.status < 400) {
+                var request = JSON.parse(req.responseText);
+                // print out the weather results
                 document.getElementById('cityResults').textContent = request.name;
                 document.getElementById('temperatureResults').textContent = request.main.temp;
                 document.getElementById('conditionResults').textContent = request.weather[0].description;
-      }
-      else{
-        console.log("Error in network request: " + req.statusText);
-      }});
-
-    req.send(null);
-
-    event.preventDefault();
-  });
+            } else {
+                console.log("Error in network request: " + req.statusText);
+            }
+        });
+        req.send(null);
+        event.preventDefault();
+    });
 }
